fce2446d12d28db9e12cc0c5c721aca9fb9585db
manodhithyaa-cs <manodhithyaa.cs@gmail.com>
2024-11-03 16:27:50 +0530

    third commit

--- utils.js
+++ utils.js
@@ -1,3 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBAEcz3vPn9Jva9upLjc9NXlfjOxJH5UQo",
+    authDomain: "chat-app-129e5.firebaseapp.com",
+    projectId: "chat-app-129e5",
+    storageBucket: "chat-app-129e5.firebasestorage.app",
+    messagingSenderId: "753486647533",
+    appId: "1:753486647533:web:712145a5e5ac2f914efce4",
+    measurementId: "G-3CFQWXDCCM"
+}
+
 const userData = {
     "userId": [],
     "username": [],
